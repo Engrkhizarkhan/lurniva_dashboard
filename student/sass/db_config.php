7f75b0ca9822465811503e7f259c5d60624b5b4b
Muhammad Khizar <engrkhizarkhan1@gmail.com>
2025-09-01 08:49:05 -0700

    db Update

--- student/sass/db_config.php
+++ student/sass/db_config.php
@@ -1,7 +1,8 @@
 <?php
 
 
-$conn = new mysqli("localhost", "root", "", "lurniva");
+// $conn = new mysqli("localhost", "root", "", "lurniva");
+$conn = new mysqli("localhost", "dashboard_user", "lurniva@testVM", "lurniva_dashboard_db"); 
 
 if ($conn->connect_error) {
   echo json_encode(['status' => 'error', 'message' => 'Database connection failed: ' . $conn->connect_error]);
