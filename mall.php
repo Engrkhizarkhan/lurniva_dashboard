8322ce94a07f5f6c43ba80dd07596bda8def2b26
Shayan1319 <shayanm1215225@gmail.com>
2025-10-03 22:48:03 +0500

    test

--- mall.php
+++ mall.php
@@ -3,7 +3,7 @@
 $to      = "yourgmail@gmail.com";   // 👉 Replace with your email
 $subject = "Test Email from Kurtlar System";
 $message = "Hello!\n\nThis is a test email from Kurtlar project (kurtlar2.2.3).\nIf you received this, email sending works fine.\n\n- Kurtlar Team";
-$from    = "noreply@yourdomain.com"; // 👉 must be a real domain email
+$from    = "support@lurniva.com"; // 👉 must be a real domain email
 $headers  = "From: $from\r\n";
 $headers .= "Reply-To: $from\r\n";
 $headers .= "X-Mailer: PHP/" . phpversion();
