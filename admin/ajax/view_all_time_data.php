d3363e65d006efa1e4a9e9650b23dea3416c30be
Shayan1319 <shayanm1215225@gmail.com>
2025-10-19 21:33:00 +0500

    update

--- admin/ajax/view_all_time_data.php
+++ admin/ajax/view_all_time_data.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_error.log');
 header('Content-Type: application/json');
 session_start(); // Important: start the session!
 require_once '../sass/db_config.php';
