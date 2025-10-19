7cdbbdb2cb1bf530cab3377f975820cf6a098694
Shayan1319 <shayanm1215225@gmail.com>
2025-10-19 21:26:08 +0500

    update

--- admin/view_all_timetable.php
+++ admin/view_all_timetable.php
@@ -1,4 +1,10 @@
-<?php require_once 'assets/php/header.php';
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_error.log');
+require_once 'assets/php/header.php';
 include_once('sass/db_config.php');
 
 if (!isset($_SESSION['admin_id'])) {
