354ef7f7dbb5cafcb298b31244b4e3c5b073ce37
Shayan1319 <shayanm1215225@gmail.com>
2025-10-20 15:10:53 +0500

    error hanlar

--- admin/sass/db_config.php
+++ admin/sass/db_config.php
@@ -8,5 +8,10 @@ if ($conn->connect_error) {
   echo json_encode(['status' => 'error', 'message' => 'Database connection failed: ' . $conn->connect_error]);
   exit;
 }
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_error.log');
 
 ?>
