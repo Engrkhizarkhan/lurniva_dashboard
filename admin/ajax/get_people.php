91d729313864b9ce75a7c3f9da4f77958dd45d38
Shayan1319 <shayanm1215225@gmail.com>
2025-10-21 19:45:32 +0500

    get people

--- admin/ajax/get_people.php
+++ admin/ajax/get_people.php
@@ -1,19 +1,23 @@
 <?php
+session_start();
 require '../sass/db_config.php';
+$admin_id = $_SESSION['admin_id'];
 
 $type = $_POST['type'];
 $options = "<option value=''>Select</option>";
 
 if ($type == 'teacher') {
-  $res = $conn->query("SELECT id, full_name FROM faculty");
-  while ($row = $res->fetch_assoc()) {
-    $options .= "<option value='{$row['id']}'>{$row['full_name']}</option>";
-  }
+    // Filter teachers by campus_id
+    $res = $conn->query("SELECT id, full_name FROM faculty WHERE campus_id = '$admin_id'");
+    while ($row = $res->fetch_assoc()) {
+        $options .= "<option value='{$row['id']}'>{$row['full_name']}</option>";
+    }
 } elseif ($type == 'student') {
-  $res = $conn->query("SELECT id, full_name FROM students");
-  while ($row = $res->fetch_assoc()) {
-    $options .= "<option value='{$row['id']}'>{$row['full_name']}</option>";
-  }
+    // Filter students by school_id
+    $res = $conn->query("SELECT id, full_name FROM students WHERE school_id = '$admin_id'");
+    while ($row = $res->fetch_assoc()) {
+        $options .= "<option value='{$row['id']}'>{$row['full_name']}</option>";
+    }
 }
 
 echo $options;
