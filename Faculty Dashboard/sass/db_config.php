d3297392d6a4369767ab3a5106a0fa3eccc0e21f
Shayan1319 <shayanm1215225@gmail.com>
2025-10-22 13:49:24 +0500

    error hanlar

--- Faculty Dashboard/sass/db_config.php
+++ Faculty Dashboard/sass/db_config.php
@@ -9,4 +9,10 @@ if ($conn->connect_error) {
   exit;
 }
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_error.log');
+
 ?>
